839e7530d238c42e3c674ad94b7de14602a8a9cb
kevin <13855268721@163.com.com>
2024-06-16 09:25:46 +0800

    feat: 测试客户端组件swr时,network有接口请求&html依赖接口数据的遍历html部分没有

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,11 +1,29 @@
 "use client";
 import Slider from "@/components/slider";
 import { Content } from "./style";
+import useSWR from "swr";
 export default function Recommend(props) {
   const { songsCount = 0 } = props;
+  // const getData = async () => {
+  //   const res = await fetch("http://localhost:3100/banner");
+  //   // console.log(res);
+  //   // The return value is *not* serialized
+  //   // You can return Date, Map, Set, etc.
+
+  //   if (!res.ok) {
+  //     // This will activate the closest `error.js` Error Boundary
+  //     throw new Error("Failed to fetch data");
+  //   }
+
+  //   return res.json();
+  // };
+  // const data = await getData();
+  const fetcher = (url) => fetch(url).then((r) => r.json());
+  const { data } = useSWR("http://localhost:3100/banner", fetcher);
+  console.log(data);
   return (
     <Content play={songsCount}>
-      <Slider bannerList={[]} />
+      <Slider bannerList={data?.banners || []} />
     </Content>
   );
 }
--- src/app/home/style.js
+++ src/app/home/style.js
@@ -1,9 +1,10 @@
-import styled from 'styled-components';
+"use client";
+import styled from "styled-components";
 
 export const Content = styled.div`
-    position: fixed;
-    top: 94px;
-    left: 0;
-    bottom: ${props => props.play > 0?"60px": 0};
-    width: 100%;
-`
+  position: fixed;
+  top: 94px;
+  left: 0;
+  bottom: ${(props) => (props.play > 0 ? "60px" : 0)};
+  width: 100%;
+`;
